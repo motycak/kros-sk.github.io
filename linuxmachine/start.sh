fcc8954e76e1c49685cdf4a3380a65194970c282
Martin Kostelej <martin010797@gmail.com>
2025-05-02 09:27:45 +0200

    small changes for script and docker compose

--- linuxmachine/start.sh
+++ linuxmachine/start.sh
@@ -4,8 +4,8 @@ set -e
 # Načítanie tokenu zo secretu
 export AZP_TOKEN=$(cat /run/secrets/azure_pat_token)
 
-if [ -z "$AZP_URL" ] || [ -z "$AZP_TOKEN" ] || [ -z "$AZP_AGENT_NAME" ]; then
-  echo 1>&2 "Musíš nastaviť AZP_URL, AZP_TOKEN (secret) a AZP_AGENT_NAME!"
+if [ -z "$AZP_URL" ] || [ -z "$AZP_TOKEN" ] || [ -z "$AZP_AGENT_NAME" ] || [ -z "$AZP_POOL" ]; then
+  echo 1>&2 "You must set AZP_URL, AZP_TOKEN (secret) and AZP_AGENT_NAME and AZP_POOL!"
   exit 1
 fi
 
@@ -16,26 +16,52 @@ cd ${WORKDIR}
 
 export AGENT_ALLOW_RUNASROOT=1
 
+print_header() {
+  lightcyan="\033[1;36m"
+  nocolor="\033[0m"
+  echo -e "\n${lightcyan}$1${nocolor}\n"
+}
+
+# Let the agent ignore the token env variables
+export VSO_AGENT_IGNORE="AZP_TOKEN"
+
 # Stiahnutie a rozbalenie agenta ak ešte neexistuje
 if [ ! -f "./config.sh" ]; then
-    curl -LsS https://vstsagentpackage.azureedge.net/agent/3.236.1/vsts-agent-linux-x64-3.236.1.tar.gz | tar -xz
+    print_header "Downloading and extracting Azure Pipelines agent..."
+    AGENT_VERSION=$(curl -s https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | jq -r '.tag_name' | sed 's/v//')
+    
+    if [ -z "$AGENT_VERSION" ] || [ "$AGENT_VERSION" == "null" ]; then
+        AGENT_VERSION="4.254.0"
+    fi
+    
+    echo "Downloading Azure Pipelines agent version $AGENT_VERSION"
+    curl -LsS "https://vstsagentpackage.azureedge.net/agent/${AGENT_VERSION}/vsts-agent-${TARGETARCH}-${AGENT_VERSION}.tar.gz" | tar -xz
 fi
+
+print_header "Configuring Azure Pipelines agent..."
 
 ./config.sh --unattended \
   --url "$AZP_URL" \
   --auth pat \
   --token "$AZP_TOKEN" \
-  --pool "${AZP_POOL:-Default}" \
+  --pool "${AZP_POOL}" \
   --agent "$AZP_AGENT_NAME" \
   --work "${WORKDIR}/_work" \
   --replace \
   --acceptTeeEula
 
 cleanup() {
-  echo "Odstraňujem agenta..."
-  ./config.sh remove --unattended --auth pat --token "$AZP_TOKEN"
+  print_header "Removing agent..."
+  while true; do
+    ./config.sh remove --unattended --auth pat --token "$AZP_TOKEN" && break
+
+    echo "Failed to remove agent, retrying in 30 seconds..."
+    sleep 30
+  done
 }
 
 trap 'cleanup; exit 130' INT TERM
+
+print_header "Starting Azure Pipelines agent..."
 
 ./run.sh 
